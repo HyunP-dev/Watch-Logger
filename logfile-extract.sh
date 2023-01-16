db5d8f8000b82cf8447561fa0eb40e13e683d603
Hyun Park <hyunp.dev@gmail.com>
2023-01-16 18:09:41 +0900

    update

--- logfile-extract.sh
+++ logfile-extract.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 echo "Watch Logger - File Extractor"
 echo "================================================"
-
+adb devices
 read -p "Device: " device
-echo ""
-echo "Available heart rate log files:"
-adb -s "$device" exec-out run-as kr.ac.hallym.watchlogger ls files
 echo "================================================"
-
+echo "List of available heart rate log files"
+adb -s "$device" exec-out run-as kr.ac.hallym.watchlogger ls files
+echo ""
 read -p "File: " filename
 adb -s "$device" exec-out run-as kr.ac.hallym.watchlogger cat files/"$filename" > "$filename"
-echo ""
-echo "Successfully extracted."
 echo "================================================"
-echo "PROGRAM ENDED."
+echo "Successfully extracted."
 echo "================================================"
